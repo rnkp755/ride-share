3c6f59962c21a1735709cbe7e0c4f0e4a8c0f3eb
Raushan Kumar Thakur <rnkp755@gmail.com>
2025-05-19 23:20:03 +0530

    Refactor post model formatting for improved readability

--- main/models/post.model.js
+++ main/models/post.model.js
@@ -2,50 +2,50 @@ import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const postSchema = new Schema(
-    {
-        userId: {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        src: {
-            type: String,
-            required: true,
-            index: true,
-        },
-        dest: {
-            type: String,
-            required: true,
-            index: true,
-        },
-        via: {
-            type: String,
-            required: true,
-        },
-        tripDate: {
-            type: String,
-            required: true,
-        },
-        tripTime: {
-            type: String,
-            required: true,
-        },
-        transportation: {
-            type: String,
-            enum: ["Bike", "Auto", "Car", "Bus", "Unknown"],
-            required: true,
-        },
-        notes: {
-            type: String,
-        },
-        visibleTo: {
-            type: String,
-            enum: ["all", "female-only", "employee-only"],
-            default: "all",
-        },
-    },
-    {timestamps: true}
-)
+	{
+		userId: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		src: {
+			type: String,
+			required: true,
+			index: true,
+		},
+		dest: {
+			type: String,
+			required: true,
+			index: true,
+		},
+		via: {
+			type: String,
+			required: true,
+		},
+		tripDate: {
+			type: String,
+			required: true,
+		},
+		tripTime: {
+			type: String,
+			required: true,
+		},
+		transportation: {
+			type: String,
+			enum: ["Bike", "Auto", "Car", "Bus", "Train", "Unknown"],
+			required: true,
+		},
+		notes: {
+			type: String,
+		},
+		visibleTo: {
+			type: String,
+			enum: ["all", "female-only", "employee-only"],
+			default: "all",
+		},
+	},
+	{ timestamps: true }
+);
 
 postSchema.plugin(mongooseAggregatePaginate);
 export const Post = mongoose.model("Post", postSchema);
